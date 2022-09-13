628f2c321f53a4faf7ebb0b5ba464494175fae73
Jacoberson <c.jacob.moore92@gmail.com>
2022-09-13 17:04:37 -0500

    Add test variable to Listeners class

--- src/test/java/com/github/jacoberson/utilities/Listeners.java
+++ src/test/java/com/github/jacoberson/utilities/Listeners.java
@@ -4,35 +4,43 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 
 public class Listeners implements ITestListener {
 	private ExtentReports extent = ExtentManager.getInstance();
+	private ExtentTest test;
 
 	@Override
 	public void onTestStart(ITestResult result) {
-		// extent.createTest(result.getMethod().getMethodName());
-		// extent.flush();
+		String methodName = result.getName();
+
+		test = extent.createTest(methodName);
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
-		extent.createTest(result.getMethod().getMethodName()).log(Status.PASS,
-				String.format("%s passed", result.getMethod().getMethodName()));
+		String methodName = result.getName();
+
+		test.log(Status.PASS, String.format("%s passed", methodName));
 		extent.flush();
 	}
 
 	@Override
 	public void onTestFailure(ITestResult result) {
-		extent.createTest(result.getMethod().getMethodName()).log(Status.FAIL,
-				String.format("%s passed", result.getMethod().getMethodName()));
+		String methodName = result.getName();
+		Throwable throwable = result.getThrowable();
+
+		test.log(Status.FAIL, String.format("%s failed with exception %s",
+				methodName, throwable));
 		extent.flush();
 	}
 
 	@Override
 	public void onTestSkipped(ITestResult result) {
-		extent.createTest(result.getMethod().getMethodName()).log(Status.SKIP,
-				String.format("%s passed", result.getMethod().getMethodName()));
+		String methodName = result.getName();
+
+		test.log(Status.SKIP, String.format("%s skipped", methodName));
 		extent.flush();
 	}
 
