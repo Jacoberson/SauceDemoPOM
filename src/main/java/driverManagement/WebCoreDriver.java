b04badccc7727e9574770c0e933a73b6ef04342a
Jacoberson <c.jacob.moore92@gmail.com>
2022-09-17 17:22:16 -0500

    Move from WebDriverManager to .exe files and remove WebDriver logs

--- src/main/java/driverManagement/WebCoreDriver.java
+++ src/main/java/driverManagement/WebCoreDriver.java
@@ -3,16 +3,24 @@ package driverManagement;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import org.apache.commons.io.output.NullOutputStream;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeDriverService;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.service.DriverService;
 
 import elementManagement.strategies.findStrategies.ClassNameFindStrategy;
 import elementManagement.strategies.findStrategies.CssSelectorFindStrategy;
@@ -20,7 +28,6 @@ import elementManagement.strategies.findStrategies.ElementFindStrategy;
 import elementManagement.strategies.findStrategies.IdFindStrategy;
 import elementManagement.strategies.findStrategies.TextFindStrategy;
 import elementManagement.strategies.findStrategies.XpathFindStrategy;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class WebCoreDriver extends Driver {
 	private static WebDriver webDriver;
@@ -35,23 +42,62 @@ public class WebCoreDriver extends Driver {
 
 	@Override
 	public void start(String browser) {
+		Logger.getLogger("org.openqa.selenium").setLevel(Level.SEVERE);
+
 		webDriver = switch (browser) {
 			case "Firefox" -> {
-				WebDriverManager.firefoxdriver().setup();
-				yield new FirefoxDriver();
+				System.setProperty("webdriver.gecko.driver",
+						"src/main/resources/browsers/geckodriver.exe");
+
+				FirefoxOptions options = new FirefoxOptions();
+				options.addArguments("--headless");
+				disableFirefoxLogs();
+
+				yield new FirefoxDriver(options);
 			}
 			case "Edge" -> {
-				WebDriverManager.edgedriver().setup();
-				yield new EdgeDriver();
+				System.setProperty("webdriver.edge.driver",
+						"src/main/resources/browsers/msedgedriver.exe");
+
+				EdgeOptions options = new EdgeOptions();
+				options.addArguments("--headless");
+				EdgeDriverService service = disableEdgeLogs();
+
+				yield new EdgeDriver(service, options);
 			}
 			default -> {
-				WebDriverManager.chromedriver().setup();
-				yield new ChromeDriver(
-						new ChromeOptions().addArguments("--headless"));
-				// yield new ChromeDriver();
+				System.setProperty("webdriver.chrome.driver",
+						"src/main/resources/browsers/chromedriver.exe");
+
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--headless");
+				ChromeDriverService service = disableChromeLogs();
+
+				yield new ChromeDriver(service, options);
 			}
 		};
 		webDriver.manage().window().maximize();
+	}
+
+	private void disableFirefoxLogs() {
+		System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,
+				"/dev/null");
+	}
+
+	private EdgeDriverService disableEdgeLogs() {
+		EdgeDriverService edgeDriverService = EdgeDriverService
+				.createDefaultService();
+		edgeDriverService.sendOutputTo(NullOutputStream.NULL_OUTPUT_STREAM);
+
+		return edgeDriverService;
+	}
+
+	private ChromeDriverService disableChromeLogs() {
+		DriverService.Builder<ChromeDriverService, ChromeDriverService.Builder> serviceBuilder = new ChromeDriverService.Builder();
+		ChromeDriverService chromeDriverService = serviceBuilder.build();
+		chromeDriverService.sendOutputTo(NullOutputStream.NULL_OUTPUT_STREAM);
+
+		return chromeDriverService;
 	}
 
 	@Override
